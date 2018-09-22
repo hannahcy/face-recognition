2bbb6dac38661515fa87c12fdbcad438dd090213
Hannah <hannah.cy@gmail.com>
2018-09-22 14:20:37 +1200

    update

--- test_network.py
+++ test_network.py
@@ -133,7 +133,7 @@ import tensorflow as tf
 
 ####### MODIFIABLE PARAMETERS ######
 
-task = "Sex"  # Options are "Sex", "Age", "Expression"
+task = "Age"  # Options are "Sex", "Age", "Expression"
 network = "vggC" # Options are "vggC", "vggD", "vggE", "vggE1"
 device = '/cpu:0' # '/cpu:0' or '/gpu:0'
 
@@ -405,7 +405,7 @@ with tf.device(device):
         # do some work with the model.
         with tf.Session() as sess:
             # Restore variables from disk.
-            saver.restore(sess, "models/sex-model_vggC_69")
+            saver.restore(sess, "models/age-model_vggC_40")
             print("Model restored.")
             test_acc = 0
             for test in range(test_batches):
