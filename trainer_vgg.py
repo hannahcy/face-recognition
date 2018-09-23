279f7078fb96836dd7f2e8798625f15a84bcbe74
Hannah <hannah.cy@gmail.com>
2018-09-23 22:55:28 +1200

    cleaned up

--- trainer_vgg.py
+++ trainer_vgg.py
@@ -187,7 +187,7 @@ import tensorflow as tf
 
 ####### MODIFIABLE PARAMETERS ######
 
-task = "Sex"  # Options are "Sex", "Age", "Expression"
+task = "Exp"  # Options are "Sex", "Age", "Expression"
 network = "vggE" # Options are "vggC", "vggD", "vggE", "vggE1"
 device = '/gpu:0' # '/cpu:0' or '/gpu:0'
 
